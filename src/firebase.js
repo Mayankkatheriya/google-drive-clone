7bc62dd89109c2b4fb6061f6c3af6a06f971f101
Mayankkatheriya <mayankkatheriya4@gmail.com>
2024-02-06 23:29:35 +0530

    firebase config hides

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDjxHSKsk9lYwEvEVHxV_S37g8DiiC7MaY",
-  authDomain: "drive-clone-6f3ee.firebaseapp.com",
-  projectId: "drive-clone-6f3ee",
-  storageBucket: "drive-clone-6f3ee.appspot.com",
-  messagingSenderId: "698212881083",
-  appId: "1:698212881083:web:b469866b3bcc67b54a4f9e"
-};
+import firebaseConfig from "./firebaseConfig";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
