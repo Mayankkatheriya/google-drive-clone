7e254201dfa5f05ae01b53e5caf2cb6bddb86be7
Mayankkatheriya <mayankkatheriya4@gmail.com>
2024-02-06 23:35:44 +0530

    config file added

---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+// firebaseConfigTemplate.js
+const firebaseConfig = {
+  apiKey: "AIzaSyDjxHSKsk9lYwEvEVHxV_S37g8DiiC7MaY",
+  authDomain: "drive-clone-6f3ee.firebaseapp.com",
+  projectId: "drive-clone-6f3ee",
+  storageBucket: "drive-clone-6f3ee.appspot.com",
+  messagingSenderId: "698212881083",
+  appId: "1:698212881083:web:b469866b3bcc67b54a4f9e",
+};
+
+export default firebaseConfig;
